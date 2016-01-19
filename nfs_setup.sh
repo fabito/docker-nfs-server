643c7e4cededf2751ece4884ae1ce79b7b92244d
Fabio Franco Uechi <fabio.uechi@gmail.com>
2016-01-19 00:03:31 -0200

    identical to kodi docs

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -6,7 +6,7 @@ mounts="${@}"
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,all_squash,insecure)" >> /etc/exports
+  echo "$src *(rw,all_squash,insecure)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
